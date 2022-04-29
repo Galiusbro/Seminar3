4f0a7d704eb460c726770456076c005cd7741686
Galprim <galprimulus@gmail.com>
2022-04-30 00:57:12 +0300

    correct Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число от 1 до 10: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= cube; i++)
+{
+    Console.WriteLine($"{i}^3 = {Math.Pow(i, 3)}");
+}
