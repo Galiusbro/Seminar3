ecf38235b10ad4b6a42f7691d7a641062518f654
Galprim <galprimulus@gmail.com>
2022-04-29 17:48:31 +0300

    add solution of tasks Seminar 3

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число на проверку полиндрома: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Poli(int num)
+{
+    int pos1 = 0, pos5 = 0, pos2 = 0, pos4 = 0;
+
+    pos1 = num / 10000;
+    pos2 = (num / 1000) % 10;
+    pos4 = (num / 10) % 10;
+    pos5 = num % 10;
+    
+    if (pos1 == pos5 && pos2 == pos4) Console.WriteLine("Число является палиндромом");
+    else Console.WriteLine("Это не палиндромом");
+    Console.WriteLine($"Проверочка: {pos1} {pos2} {pos4} {pos5}");
+}
+
+Poli(number);
---
+++ Task21/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход 
+//координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+void FillPoint(int[] point)
+{
+    int length = point.Length;
+    int index = 0;
+    while (index < length)
+    {
+        point[index] = new Random().Next(-10, 11);
+        index++;
+    }
+}
+
+void PrintPoint(int[] pointPos)
+{
+    int count = pointPos.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{pointPos[position]} ");
+        position++;
+    }
+    Console.WriteLine();
+}
+
+void PointDist(int[] point1, int[] point2)
+{
+    double dis = Math.Sqrt  (
+        (point2[0]-point1[0])*(point2[0]-point1[0])+
+        (point2[1]-point1[1])*(point2[1]-point1[1])+
+        (point2[2]-point1[0])*(point2[0]-point1[2])
+                            );
+    Console.WriteLine("{0:f}", dis);                        
+}
+
+int[] piont1 = new int[3];
+int[] piont2 = new int[3];
+
+FillPoint(piont1);
+PrintPoint(piont1);
+
+FillPoint(piont2);
+PrintPoint(piont2);
+
+PointDist(piont1, piont2);
